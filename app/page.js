e65155e2fc5b8e95835059b1a3c7d486804b0f72
TamimOp <tamimshadanik@gmail.com>
2025-07-28 17:19:31 +0600

    Enhance Home component layout with styled divs for better visual structure

--- app/page.js
+++ app/page.js
@@ -3,9 +3,20 @@ import HeroSection from "@/components/HeroFinance";
 export default function Home() {
   return (
     <main>
-      <div style={{ height: "100vh" }} />
+      <div
+        style={{ height: "100vh" }}
+        className="flex items-center justify-center text-9xl font-bold text-[#34C771]"
+      >
+        Scroll Down
+      </div>
       <HeroSection />
-      <div style={{ height: "100vh" }} />
+      <div
+        style={{ height: "100vh" }}
+        className="flex items-center justify-center text-9xl font-bold text-[#477EE9]"
+      >
+        {" "}
+        Finish{" "}
+      </div>
     </main>
   );
 }
